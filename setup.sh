d6869a4eb2252ba7ac1939acc35c5a888f0d4636
mrdaniblu <mr.daniblu@gmail.com>
2023-09-13 11:32:58 +0200

    setup file

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# git config
+git config --global user.email "mr.daniblu@gmail.com"
+git config --global user.name "mrdaniblu"
+
+# install venv
+sudo apt-get update
+sudo apt-get install python3-venv
+
+# create new kernel using venv
+# python3 -m ipykernel install --user --name=nlp_e23
+
+# activate env
+source ../venvs/nlp_e23/bin/activate
