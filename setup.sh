b34613c9a242daea8c35c8d3ac62d0131a2f587a
mrdaniblu <mr.daniblu@gmail.com>
2023-09-13 12:04:03 +0200

    requirements added

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ git config --global user.name "mrdaniblu"
 sudo apt-get update
 sudo apt-get install python3-venv
 
-# create new kernel using venv
-# python3 -m ipykernel install --user --name=nlp_e23
-
 # activate env
-source ../venvs/nlp_e23/bin/activate
+source ../venvs/nlp-e23/bin/activate
+
+# create new kernel using venv
+python3 -m ipykernel install --user --name=nlp-e23
